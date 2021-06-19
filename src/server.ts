1c6dd143180a0f1078ef2dd3c41339a298ef2cf2
Dhemes Mota <dhemes.mota@gmail.com>
2021-06-19 15:47:42 -0300

    add ts-node-dev!

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "Hello World" }));
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello Dev" });
+});
 
-app.listen(33333);
+app.listen(3333, () => console.log("Server is running!"));
